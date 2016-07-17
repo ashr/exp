01257e4a9280674ab08cd6fbcaf94272e339d327
mewmew <rnd0x00@gmail.com>
2016-07-17 16:05:48 +0200

    sym/testdata: Add types.sym containing debug symbols for each basic type.

---
+++ sym/testdata/types/types.c
@@ -0,0 +1,26 @@
+typedef struct struct_name {
+	char x;
+	int y;
+} struct_type;
+
+typedef enum enum_name {
+	A = 1,
+	B = 2,
+} enum_type;
+
+long long v_0000;
+void v_0001;
+char v_0002;
+short v_0003;
+int v_0004;
+long v_0005;
+float v_0006;
+double v_0007;
+struct_type v_0008;
+//? v_0009;
+enum_type v_000A;
+//? v_000B;
+unsigned char v_000C;
+unsigned short v_000D;
+unsigned int v_000E;
+unsigned long v_000F;
