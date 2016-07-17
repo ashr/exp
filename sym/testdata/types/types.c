4962a9ff1ffdd68bfcde06f13d2bcc3ba315ff6a
mewmew <rnd0x00@gmail.com>
2016-07-17 17:42:08 +0200

    sym/testdata: Add pointers, arrays and function pointers to types.sym.

--- sym/testdata/types/types.c
+++ sym/testdata/types/types.c
@@ -1,3 +1,5 @@
+// Basic types.
+
 typedef struct struct_name {
 	char x;
 	int y;
@@ -24,3 +26,62 @@ unsigned char v_000C;
 unsigned short v_000D;
 unsigned int v_000E;
 unsigned long v_000F;
+
+// Derived types.
+
+// Pointers.
+
+long long * v_0010;
+void * v_0011;
+char * v_0012;
+short * v_0013;
+int * v_0014;
+long * v_0015;
+float * v_0016;
+double * v_0017;
+struct_type * v_0018;
+//? * v_0019;
+enum_type * v_001A;
+//? * v_001B;
+unsigned char * v_001C;
+unsigned short * v_001D;
+unsigned int * v_001E;
+unsigned long * v_001F;
+
+// Arrays.
+
+long long v_0030[1];
+//void v_0031[1];
+char v_0032[1];
+short v_0033[1];
+int v_0034[1];
+long v_0035[1];
+float v_0036[1];
+double v_0037[1];
+struct_type v_0038[1];
+//? v_0039[1];
+enum_type v_003A[1];
+//? v_003B[1];
+unsigned char v_003C[1];
+unsigned short v_003D[1];
+unsigned int v_003E[1];
+unsigned long v_003F[1];
+
+// Function pointers.
+
+long long (*v_0090)();
+void (*v_0091)();
+char (*v_0092)();
+short (*v_0093)();
+int (*v_0094)();
+long (*v_0095)();
+float (*v_0096)();
+double (*v_0097)();
+struct_type (*v_0098)();
+//? (*v_0099)();
+enum_type (*v_009A)();
+//? (*v_009B)();
+unsigned char (*v_009C)();
+unsigned short (*v_009D)();
+unsigned int (*v_009E)();
+unsigned long (*v_009F)();
