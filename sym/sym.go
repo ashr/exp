9352969f9cdde5b79f95a2500de445f170ccf4db
mewmew <rnd0x00@gmail.com>
2016-08-09 20:14:32 +0200

    sym: Add definition of types and start work on symbol data.

--- sym/sym.go
+++ sym/sym.go
@@ -92,32 +92,34 @@ func parseHeader(r io.Reader) error {
 
 // parseSymbol parses a debug symbol of the given PS1 symbol file.
 func parseSymbol(r io.Reader) (Symbol, error) {
-	// Parse symbol offset.
-	var offset uint32
-	if err := binary.Read(r, binary.LittleEndian, &offset); err != nil {
+	// Parse symbol value.
+	var value uint32
+	if err := binary.Read(r, binary.LittleEndian, &value); err != nil {
 		if err == io.EOF {
 			return nil, io.EOF
 		}
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("offset: %08X", offset)
+	dbg.Printf("value: %08X", value)
 
 	// Symbol kind.
 	//
 	//    0x01 = compiler and C runtime definitions.
 	//    0x02 = global variable or function address.
+	//    0x06 = class method?
 	//    0x80 = line marker
 	//    0x82 = ?
 	//    0x84 = ?
 	//    0x86 = ?
 	//    0x88 = named source file.
 	//    0x8A = unnamed source file.
-	//    0x8C = named source file; function definition.
-	//    0x8E = line number of function definition.
-	//    0x90 = ?
-	//    0x92 = ?
-	//    0x94 = global variable, local variable, function, enumerable type,
-	//           enumerable member, structure type or structure field definition.
+	//    0x8C = function definition start.
+	//    0x8E = function definition stop.
+	//    0x90 = local variable definition start.
+	//    0x92 = local variable definition stop.
+	//    0x94 = definition; such as global variable, local variable, function,
+	//           enumerable type, enumerable member, structure type or structure
+	//           field definition.
 	//    0x96 = meta symbol; key-value pair.
 	//    0x98 = ?
 	//    0x9A = ?
@@ -171,102 +173,103 @@ func parseSymbol(r io.Reader) (Symbol, error) {
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
-	return &symbol{offset: offset, kind: kind, data: data}, nil
+	return &symbol{value: value, kind: kind, data: data}, nil
 }
 
 // parseSymbol01 parses a debug symbol of kind 0x01.
-func parseSymbol01(r io.Reader) (*data01, error) {
+func parseSymbol01(r io.Reader) (*Data01, error) {
 	s, err := readString(r)
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Println("s:", s)
-	return &data01{}, nil
+	return &Data01{}, nil
 }
 
 // parseSymbol02 parses a debug symbol of kind 0x02.
-func parseSymbol02(r io.Reader) (*data02, error) {
+func parseSymbol02(r io.Reader) (*Data02, error) {
 	// Parse identifier name.
 	name, err := readString(r)
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Println("name:", name)
-	return &data02{}, nil
+	return &Data02{}, nil
 }
 
 // parseSymbol80 parses a debug symbol of kind 0x80.
-func parseSymbol80(r io.Reader) (*data80, error) {
+func parseSymbol80(r io.Reader) (*Data80, error) {
 	// nothing to do.
 	return nil, nil
 }
 
 // parseSymbol82 parses a debug symbol of kind 0x82.
-func parseSymbol82(r io.Reader) (*data82, error) {
+func parseSymbol82(r io.Reader) (*Data82, error) {
 	// Read unknown data.
 	buf := make([]byte, 1)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Printf("symbol 0x82 data: % X", buf)
-	return &data82{}, nil
+	return &Data82{}, nil
 }
 
 // parseSymbol84 parses a debug symbol of kind 0x84.
-func parseSymbol84(r io.Reader) (*data84, error) {
+func parseSymbol84(r io.Reader) (*Data84, error) {
 	// Read unknown data.
 	buf := make([]byte, 2)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Printf("symbol 0x84 data: % X", buf)
-	return &data84{}, nil
+	return &Data84{}, nil
 }
 
 // parseSymbol86 parses a debug symbol of kind 0x86.
-func parseSymbol86(r io.Reader) (*data86, error) {
+func parseSymbol86(r io.Reader) (*Data86, error) {
 	// Read unknown data.
 	buf := make([]byte, 4)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Printf("symbol 0x86 data: % X", buf)
-	return &data86{}, nil
+	return &Data86{}, nil
 }
 
 // parseSymbol88 parses a debug symbol of kind 0x88.
-func parseSymbol88(r io.Reader) (*data88, error) {
+func parseSymbol88(r io.Reader) (*Data88, error) {
 	// Parse object count.
-	var nobjs uint32
-	if err := binary.Read(r, binary.LittleEndian, &nobjs); err != nil {
+	data := new(Data88)
+	if err := binary.Read(r, binary.LittleEndian, &data.NObjs); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("nobjs: %d", nobjs)
+	dbg.Printf("nobjs: %d", data.NObjs)
 
 	// Parse source path.
 	path, err := readString(r)
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
+	data.Path = path
 	dbg.Println("path:", path)
 
-	return &data88{nobjs: nobjs, path: path}, nil
+	return data, nil
 }
 
 // parseSymbol8A parses a debug symbol of kind 0x8A.
-func parseSymbol8A(r io.Reader) (*data8A, error) {
+func parseSymbol8A(r io.Reader) (*Data8A, error) {
 	// nothing to do.
 	return nil, nil
 }
 
 // parseSymbol8C parses a debug symbol of kind 0x8C.
-func parseSymbol8C(r io.Reader) (*data8C, error) {
+func parseSymbol8C(r io.Reader) (*Data8C, error) {
+	data := new(Data8C)
 	// Read unknown data.
-	buf := make([]byte, 20)
-	if _, err := io.ReadFull(r, buf); err != nil {
+	if _, err := io.ReadFull(r, data.Unknown[:]); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("symbol 0x8C data: % X", buf)
+	dbg.Printf("symbol 0x8C data: % X", data.Unknown[:])
 
 	// Parse source path.
 	path, err := readString(r)
@@ -274,6 +277,7 @@ func parseSymbol8C(r io.Reader) (*data8C, error) {
 		return nil, errutil.Err(err)
 	}
 	dbg.Println("path:", path)
+	data.Path = path
 
 	// Parse function name.
 	name, err := readString(r)
@@ -281,45 +285,46 @@ func parseSymbol8C(r io.Reader) (*data8C, error) {
 		return nil, errutil.Err(err)
 	}
 	dbg.Println("name:", name)
+	data.Name = name
 
-	return &data8C{}, nil
+	return data, nil
 }
 
 // parseSymbol8E parses a debug symbol of kind 0x8E.
-func parseSymbol8E(r io.Reader) (*data8E, error) {
+func parseSymbol8E(r io.Reader) (*Data8E, error) {
 	// Parse object count.
-	var line uint32
-	if err := binary.Read(r, binary.LittleEndian, &line); err != nil {
+	data := new(Data8E)
+	if err := binary.Read(r, binary.LittleEndian, &data.Line); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("line: %d", line)
-	return &data8E{line: line}, nil
+	dbg.Printf("line: %d", data.Line)
+	return data, nil
 }
 
 // parseSymbol90 parses a debug symbol of kind 0x90.
-func parseSymbol90(r io.Reader) (*data90, error) {
+func parseSymbol90(r io.Reader) (*Data90, error) {
 	// Read unknown data.
 	buf := make([]byte, 4)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Printf("symbol 0x90 data: % X", buf)
-	return &data90{}, nil
+	return &Data90{}, nil
 }
 
 // parseSymbol92 parses a debug symbol of kind 0x92.
-func parseSymbol92(r io.Reader) (*data92, error) {
+func parseSymbol92(r io.Reader) (*Data92, error) {
 	// Read unknown data.
 	buf := make([]byte, 4)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Printf("symbol 0x92 data: % X", buf)
-	return &data92{}, nil
+	return &Data92{}, nil
 }
 
 // parseSymbol94 parses a debug symbol of kind 0x94.
-func parseSymbol94(r io.Reader) (*data94, error) {
+func parseSymbol94(r io.Reader) (*Data94, error) {
 	// Definition kind.
 	//
 	//    0001 = local variable definition.
@@ -331,13 +336,13 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 	//    000F = enum type definition.
 	//    0010 = enum member definition.
 	//    0066 = end of symbol marker.
-	var defKind uint16
 
 	// Parse definition kind.
-	if err := binary.Read(r, binary.LittleEndian, &defKind); err != nil {
+	data := new(Data94)
+	if err := binary.Read(r, binary.LittleEndian, &data.DefKind); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("defKind: %04X", defKind)
+	dbg.Printf("defKind: %04X", data.DefKind)
 
 	// Type. A type is made up of a 4-bit basic type specifyer, and a set of
 	// 2-bit type modifiers.
@@ -382,7 +387,7 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 	//
 	//                                  0x94 = 00 00 00 00 10 01 0100
 
-	//    0000 = long long
+	//    0000 = error
 	//    0001 = void
 	//    0002 = char
 	//    0003 = short
@@ -391,15 +396,15 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 	//    0006 = float
 	//    0007 = double
 	//    0008 = struct_type
-	//    0009 = ?
+	//    0009 = union
 	//    000A = enum_type
-	//    000B = ? (enum member)
+	//    000B = enum_member
 	//    000C = unsigned char
 	//    000D = unsigned short
 	//    000E = unsigned int
 	//    000F = unsigned long
 
-	//    0010 = long long *
+	//    0010 = error *
 	//    0011 = void *
 	//    0012 = char *
 	//    0013 = short *
@@ -408,15 +413,15 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 	//    0016 = float *
 	//    0017 = double *
 	//    0018 = struct_type *
-	//    0019 = ? *
+	//    0019 = union *
 	//    001A = enum_type *
-	//    001B = ? *
+	//    001B = enum_member *
 	//    001C = unsigned char *
 	//    001D = unsigned short *
 	//    001E = unsigned int *
 	//    001F = unsigned long *
 
-	//    0020 = long long function
+	//    0020 = error function
 	//    0021 = void function
 	//    0022 = char function
 	//    0023 = short function
@@ -425,15 +430,15 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 	//    0026 = float function
 	//    0027 = double function
 	//    0028 = struct_type function
-	//    0029 = ? function
+	//    0029 = union function
 	//    002A = enum_type function
-	//    002B = ? function
+	//    002B = enum_member function
 	//    002C = unsigned char function
 	//    002D = unsigned short function
 	//    002E = unsigned int function
 	//    002F = unsigned long function
 
-	//    0030 = long long array
+	//    0030 = error array
 	//    0031 = void array
 	//    0032 = char array
 	//    0033 = short array
@@ -442,27 +447,25 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 	//    0036 = float array
 	//    0037 = double array
 	//    0038 = struct_type array
-	//    0039 = ? array
+	//    0039 = union array
 	//    003A = enum_type array
-	//    003B = ? array
+	//    003B = enum_member array
 	//    003C = unsigned char array
 	//    003D = unsigned short array
 	//    003E = unsigned int array
 	//    003F = unsigned long array
-	var typ uint16
 
 	// Parse type.
-	if err := binary.Read(r, binary.LittleEndian, &typ); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, &data.Type); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("typ: %04X", typ)
+	dbg.Printf("typ: %04X;    %v", uint16(data.Type), data.Type)
 
 	// Read unknown data.
-	buf := make([]byte, 4)
-	if _, err := io.ReadFull(r, buf); err != nil {
+	if _, err := io.ReadFull(r, data.Unknown[:]); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("symbol 0x94 data: % X", buf)
+	dbg.Printf("symbol 0x94 data: % X", data.Unknown[:])
 
 	// Parse identifier name.
 	name, err := readString(r)
@@ -470,80 +473,81 @@ func parseSymbol94(r io.Reader) (*data94, error) {
 		return nil, errutil.Err(err)
 	}
 	dbg.Println("name:", name)
+	data.Name = name
 
-	return &data94{}, nil
+	return data, nil
 }
 
 // parseSymbol96 parses a debug symbol of kind 0x96.
-func parseSymbol96(r io.Reader) (*data96, error) {
-	data := new(data96)
+func parseSymbol96(r io.Reader) (*Data96, error) {
+	data := new(Data96)
 	// Parse definition kind.
-	if err := binary.Read(r, binary.LittleEndian, &data.defKind); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, &data.DefKind); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("defKind: %04X", data.defKind)
+	dbg.Printf("defKind: %04X", data.DefKind)
 
 	// Parse type.
-	if err := binary.Read(r, binary.LittleEndian, &data.typ); err != nil {
+	if err := binary.Read(r, binary.LittleEndian, &data.Type); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("typ: %04X", data.typ)
+	dbg.Printf("typ: %04X;    %v", uint16(data.Type), data.Type)
 
 	// Read unknown data.
-	if _, err := io.ReadFull(r, data.unknown[:]); err != nil {
+	if _, err := io.ReadFull(r, data.Unknown[:]); err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Printf("symbol 0x96 data: % X", data.unknown[:])
+	dbg.Printf("symbol 0x96 data: % X", data.Unknown[:])
 
 	// Parse array lengths.
-	n := getNArrays(data.typ)
+	n := getNArrays(data.Type)
 	for i := 0; i < n; i++ {
 		// Parse type.
 		var length uint32
 		if err := binary.Read(r, binary.LittleEndian, &length); err != nil {
 			return nil, errutil.Err(err)
 		}
-		data.lengths = append(data.lengths, length)
+		data.Lengths = append(data.Lengths, length)
 		dbg.Printf("length: %d", length)
 	}
 
 	// Parse key.
 	var err error
-	data.key, err = readString(r)
+	data.Key, err = readString(r)
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Println("key:", data.key)
+	dbg.Println("key:", data.Key)
 
 	// Parse value.
-	data.val, err = readString(r)
+	data.Val, err = readString(r)
 	if err != nil {
 		return nil, errutil.Err(err)
 	}
-	dbg.Println("val:", data.val)
+	dbg.Println("val:", data.Val)
 
 	return data, nil
 }
 
 // parseSymbol98 parses a debug symbol of kind 0x98.
-func parseSymbol98(r io.Reader) (*data98, error) {
+func parseSymbol98(r io.Reader) (*Data98, error) {
 	// Read unknown data.
 	buf := make([]byte, 8)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return nil, errutil.Err(err)
 	}
 	dbg.Printf("symbol 0x98 data: % X", buf)
-	return &data98{}, nil
+	return &Data98{}, nil
 }
 
 // parseSymbol9A parses a debug symbol of kind 0x9A.
-func parseSymbol9A(r io.Reader) (*data9A, error) {
+func parseSymbol9A(r io.Reader) (*Data9A, error) {
 	// nothing to do.
 	return nil, nil
 }
 
 // getNArrays returns the number of arrays contained within the given type.
-func getNArrays(typ uint16) int {
+func getNArrays(typ Type) int {
 	n := 0
 	for x := typ >> 4; x != 0; x >>= 2 {
 		// Check if array modifier (0b11).
--- sym/symbol.go
+++ sym/symbol.go
@@ -1,19 +1,23 @@
 package sym
 
+import "fmt"
+
 // TODO: Figure out how to access symbol specific data.
 
 // A Symbol represents a debug symbol.
 type Symbol interface {
-	// Offset returns the offset of the symbol.
-	Offset() uint32
+	// Value returns the value of the symbol.
+	Value() uint32
 	// Kind returns the symbol kind.
 	Kind() uint8
+	// Data returns symbol specific data.
+	Data() interface{}
 }
 
 // A symbol represents a debug symbol.
 type symbol struct {
-	// Symbol offset.
-	offset uint32
+	// Symbol value.
+	value uint32
 	// Symbol kind.
 	kind uint8
 	// Holds symbol specific data, which has one of the following underlying
@@ -24,9 +28,9 @@ type symbol struct {
 	data interface{}
 }
 
-// Offset returns the offset of the symbol.
-func (s symbol) Offset() uint32 {
-	return s.offset
+// Value returns the value of the symbol.
+func (s symbol) Value() uint32 {
+	return s.value
 }
 
 // Kind returns the symbol kind.
@@ -34,74 +38,226 @@ func (s symbol) Kind() uint8 {
 	return s.kind
 }
 
-type data01 struct {
+// Data returns symbol specific data.
+func (s symbol) Data() interface{} {
+	return s.data
 }
 
-type data02 struct {
+type Data01 struct {
+}
+
+type Data02 struct {
 }
 
 // Symbol kind 0x80 represents a line marker within the source file.
-type data80 struct {
+type Data80 struct {
 	// no data.
 }
 
-type data82 struct {
+type Data82 struct {
 }
 
-type data84 struct {
+type Data84 struct {
 }
 
-type data86 struct {
+type Data86 struct {
 }
 
 // Symbol kind 0x88 represents a named source file start marker.
-type data88 struct {
+type Data88 struct {
 	// Number of objects contained within the source file.
-	nobjs uint32
+	NObjs uint32
 	// Souce file path.
-	path string
+	Path string
 }
 
-type data8A struct {
+type Data8A struct {
 }
 
-type data8C struct {
+// Symbol kind 0x8C represents the start marker of a function declaration.
+type Data8C struct {
+	// Unknown data.
+	Unknown [20]byte
+	// Source code file path.
+	Path string
+	// Function name.
+	Name string
 }
 
 // Symbol kind 0x8E represents a line number marker associated with the directly
 // preceding function definition.
-type data8E struct {
+type Data8E struct {
 	// Line number.
-	line uint32
+	Line uint32
 }
 
-type data90 struct {
+type Data90 struct {
 }
 
-type data92 struct {
+type Data92 struct {
 }
 
-type data94 struct {
+// Symbol kind 0x94 represents a definition of a function, struct, variable,
+// parameter or union.
+type Data94 struct {
+	// Definition kind.
+	DefKind uint16
+	// Type.
+	Type Type
+	// Unknown data.
+	Unknown [4]byte
+	// Symbol name.
+	Name string
 }
 
 // Symbol kind 0x96 represents a key-value pair meta-symbol, used to describe
 // end of symbol markers, type aliases, variable definitions of structure,
 // enumerable or array types.
-type data96 struct {
+type Data96 struct {
 	// Definition kind.
-	defKind uint16
+	DefKind uint16
 	// Type.
-	typ uint16
+	Type Type
 	// Unknown data.
-	unknown [6]byte
+	Unknown [6]byte
 	// Array lengths.
-	lengths []uint32
+	Lengths []uint32
 	// Key-value pair.
-	key, val string
+	Key, Val string
 }
 
-type data98 struct {
+// A Type represents a C type, where the 4 least significant bits represent a
+// basic type and each pair of 2 bits of the remaining 12 bits represent a type
+// modifier.
+type Type uint16
+
+// String returns the string representation of the type.
+func (t Type) String() string {
+	typ := uint16(t)
+	s := ""
+	basic := basicString(typ & 0x000F)
+	s += basic
+	mask := (typ & 0xFFF0) >> 4
+	wat := false
+	for i := 0; i < 6; i++ {
+		mod := mask & 0x3
+		if mod == ModNone {
+			wat = true
+			continue
+		}
+		m := modString(mod)
+		if wat {
+			panic("WAT?")
+		}
+		s = m + "(" + s + ")"
+		mask >>= 2
+	}
+	return s
 }
 
-type data9A struct {
+const (
+	TypeError         = 0x0 // error
+	TypeVoid          = 0x1 // void
+	TypeChar          = 0x2 // char
+	TypeShort         = 0x3 // short
+	TypeInt           = 0x4 // int
+	TypeLong          = 0x5 // long
+	TypeFloat         = 0x6 // float
+	TypeDouble        = 0x7 // double
+	TypeStruct        = 0x8 // struct
+	TypeUnion         = 0x9 // union
+	TypeEnum          = 0xA // enum
+	TypeEnumMember    = 0xB // enum member
+	TypeUnsignedChar  = 0xC // unsigned char
+	TypeUnsignedShort = 0xD // unsigned short
+	TypeUnsignedInt   = 0xE // unsigned int
+	TypeUnsignedLong  = 0xF // unsigned long
+)
+
+const (
+	ModNone     = 0x0 // basic type
+	ModPointer  = 0x1 // pointer
+	ModFunction = 0x2 // function
+	ModArray    = 0x3 // array
+)
+
+// HasMod reports whether the given type has the specified type modifier.
+func HasMod(typ Type, modifier uint16) bool {
+	mask := (typ & 0xFFF0) >> 4
+	wat := false
+	for i := 0; i < 6; i++ {
+		mod := mask & 0x3
+		if mod == ModNone {
+			wat = true
+			continue
+		}
+		if wat {
+			panic("WAT?")
+		}
+		if uint16(mod) == modifier {
+			return true
+		}
+		mask >>= 2
+	}
+	return false
+}
+
+// modString returns the string representation of the givne type modifier.
+func modString(mod uint16) string {
+	switch mod {
+	case ModNone:
+		return "basic type"
+	case ModPointer:
+		return "pointer"
+	case ModFunction:
+		return "function"
+	case ModArray:
+		return "array"
+	}
+	panic(fmt.Sprintf("unknown type modifier %v", mod))
+}
+
+// basicString returns the string representation of the givne basic type.
+func basicString(basic uint16) string {
+	switch basic {
+	case TypeError:
+		return "error"
+	case TypeVoid:
+		return "void"
+	case TypeChar:
+		return "char"
+	case TypeShort:
+		return "short"
+	case TypeInt:
+		return "int"
+	case TypeLong:
+		return "long"
+	case TypeFloat:
+		return "float"
+	case TypeDouble:
+		return "double"
+	case TypeStruct:
+		return "struct"
+	case TypeUnion:
+		return "union"
+	case TypeEnum:
+		return "enum"
+	case TypeEnumMember:
+		return "enum member"
+	case TypeUnsignedChar:
+		return "unsigned char"
+	case TypeUnsignedShort:
+		return "unsigned short"
+	case TypeUnsignedInt:
+		return "unsigned int"
+	case TypeUnsignedLong:
+		return "unsigned long"
+	}
+	panic(fmt.Sprintf("unknown basic type %v", basic))
+}
+
+type Data98 struct {
+}
+
+type Data9A struct {
 }
