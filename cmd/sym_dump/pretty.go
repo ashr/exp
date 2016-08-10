af73a132d82296d9a818460288679ac3f699e2dd
mewmew <rnd0x00@gmail.com>
2016-08-10 02:30:32 +0200

    cmd/sym_dump: Pretty, don't forget pretty.

---
+++ cmd/sym_dump/pretty.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func pretty(fs map[string]*FuncDecl, globals map[string]*Var, types map[string]Type, fOrder, globalOrder, aliasOrder []string) {
+	/*
+		for _, name := range aliasOrder {
+			typeName := alias[name]
+			t := types[typeName]
+			fmt.Println(t)
+		}
+	*/
+
+	/*
+		for _, name := range globalOrder {
+			global := globals[name]
+			fmt.Printf("%s;\n", global)
+		}
+	*/
+
+	if err := os.MkdirAll("_dump_", 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, name := range fOrder {
+		f := fs[name]
+		if len(f.Path) == 0 {
+			fmt.Printf("// %s\n", f.Path)
+			fmt.Println(f)
+			fmt.Println()
+			continue
+		}
+		path := strings.ToLower(f.Path)
+		path = filepath.Clean(strings.Replace(path, "\\", "/", -1))
+		path = path[len("c:/"):]
+		dir, name := filepath.Split(path)
+		dir = filepath.Join("_dump_", dir)
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			log.Fatal(err)
+		}
+		path = filepath.Join(dir, name)
+		file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := file.WriteString(f.String() + "\n\n"); err != nil {
+			log.Fatal(err)
+		}
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
