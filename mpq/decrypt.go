4fbbd690081ac7c88eec7283e58d1713519d731c
mewmew <rnd0x00@gmail.com>
2017-05-13 23:56:12 +0200

    mpq: implement Open to parse MPQ headers

---
+++ mpq/decrypt.go
@@ -0,0 +1,157 @@
+//go:generate go run gen.go
+
+package mpq
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"path"
+	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	// Lookup table used during decryption.
+	cryptTable [0x500]uint32
+	// Key used to decrypt the hash table.
+	hashTableKey uint32
+	// Key used to decrypt the block table.
+	blockTableKey uint32
+)
+
+func init() {
+	// Initialize crypt table.
+	initCryptTable()
+
+	// initialize hash table key and block table key.
+	hashTableKey = fileKey("(hash table)")
+	blockTableKey = fileKey("(block table)")
+
+	// Initialize file listings.
+	if err := initListings(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+}
+
+// initCryptTable initializes the lookup table used during decryption.
+func initCryptTable() {
+	//start := time.Now()
+	seed := uint32(0x00100001)
+	for index1 := 0; index1 < 0x100; index1++ {
+		index2 := index1
+		for i := 0; i < 5; i++ {
+			seed = (seed*125 + 3) % 0x2AAAAB
+			temp1 := (seed & 0xFFFF) << 0x10
+			seed = (seed*125 + 3) % 0x2AAAAB
+			temp2 := (seed & 0xFFFF)
+			cryptTable[index2] = (temp1 | temp2)
+			index2 += 0x100
+		}
+	}
+	// Takes on average 60.00 µs.
+	//fmt.Println("init of crypt tables took:", time.Since(start))
+}
+
+// decrypt decrypts the source data in-place with the given key.
+func decrypt(src []byte, key uint32) []byte {
+	// Preprocess; convert []byte to []uint32.
+	xs := make([]uint32, len(src)/4)
+	for i := range xs {
+		xs[i] = binary.LittleEndian.Uint32(src[i*4:])
+	}
+
+	seed := uint32(0xEEEEEEEE)
+	for i := range xs {
+		seed += cryptTable[0x400+(key&0xFF)]
+		xs[i] ^= key + seed
+		key = ((^key << 0x15) + 0x11111111) | (key >> 0x0B)
+		seed = xs[i] + seed + (seed << 5) + 3
+	}
+
+	// Preprocess; convert []uint32 to []byte.
+	for i, x := range xs {
+		binary.LittleEndian.PutUint32(src[i*4:], x)
+	}
+	return src
+}
+
+// hashType represents the set of hash types.
+type hashType uint32
+
+// Hash types.
+const (
+	// Hash of relative file path, which specifies an index hash the hash table,
+	// from where to start searching for the hash entry associated with the given
+	// file.
+	hashTableIndex hashType = 0x000
+	// Hash of relative file path, using method A.
+	hashPathA hashType = 0x100
+	// Hash of relative file path, using method B.
+	hashPathB hashType = 0x200
+	// Hash of the file name, which specifies the encryption key of the file.
+	hashFileKey hashType = 0x300
+)
+
+// hash returns the hash of the given string, based on the specified hash type.
+func hash(s string, hashType hashType) uint32 {
+	s = strings.ToUpper(s)
+	seed1 := uint32(0x7FED7FED)
+	seed2 := uint32(0xEEEEEEEE)
+	for i := 0; i < len(s); i++ {
+		v := uint32(s[i])
+		seed1 = cryptTable[uint32(hashType)+v] ^ (seed1 + seed2)
+		seed2 = v + seed1 + seed2 + (seed2 << 5) + 3
+	}
+	return seed1
+}
+
+// fileKey returns the encryption key of the given file.
+func fileKey(relPath string) uint32 {
+	relPath = strings.Replace(relPath, "\\", "/", -1)
+	name := path.Base(relPath)
+	return hash(name, hashFileKey)
+}
+
+// listings maps file hashes to their original file paths.
+var listings map[hashAB]string
+
+// hashAB contains both the A and B hashes of a given file, to be used as key
+// for file listings.
+type hashAB struct {
+	// Hash of relative file path; using method A.
+	hashA uint32
+	// Hash of relative file path; using method B.
+	hashB uint32
+}
+
+// initListings initializes the file listings.
+func initListings() error {
+	//start := time.Now()
+	s := bufio.NewScanner(strings.NewReader(listfile))
+	var paths []string
+	for s.Scan() {
+		path := s.Text()
+		paths = append(paths, path)
+	}
+	if err := s.Err(); err != nil {
+		return errors.WithStack(err)
+	}
+	sort.Strings(paths)
+	listings = make(map[hashAB]string)
+	for _, path := range paths {
+		hashA := hash(path, hashPathA)
+		hashB := hash(path, hashPathB)
+		key := hashAB{hashA: hashA, hashB: hashB}
+		if prev, ok := listings[key]; ok {
+			panic(fmt.Errorf("hash collision between %q and %q", prev, path))
+		}
+		listings[key] = path
+	}
+	// Takes on average 20.00 ms.
+	//fmt.Println("init of listings took:", time.Since(start))
+	return nil
+}
--- mpq/mpq.go
+++ mpq/mpq.go
@@ -1,13 +1,81 @@
 // Package mpq provides access to MPQ archives.
 package mpq
 
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"io/ioutil"
+	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 // An Archive is an MPQ archive.
 type Archive struct {
+	// MPQ header.
+	header
+	// Hash entries.
+	hashes []hashEntry
+	// Block entries.
+	blocks []blockEntry
+	// Underlying io.ReaderAt of the archive.
+	r io.ReaderAt
 }
 
 // Open opens the given MPQ archive.
 func Open(mpqPath string) (*Archive, error) {
-	panic("not yet implemented")
+	// Open MPQ archive.
+	buf, err := ioutil.ReadFile(mpqPath)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	r := bytes.NewReader(buf)
+	a := &Archive{
+		r: r,
+	}
+
+	// Parse MPQ header.
+	if err := binary.Read(r, binary.LittleEndian, &a.header); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	want := "MPQ\x1A"
+	got := string(a.Magic[:])
+	if got != want {
+		return nil, errors.Errorf("magic header mismatch; expected %q, got %q", want, got)
+	}
+
+	// Decrypt hash table.
+	const hashEntrySize = 16
+	n := hashEntrySize * int64(a.HashCount)
+	enc := make([]byte, n)
+	if _, err := a.r.ReadAt(enc, int64(a.HashTableOffset)); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	dec := decrypt(enc, hashTableKey)
+
+	// Parse hash table.
+	a.hashes = make([]hashEntry, a.HashCount)
+	if err := binary.Read(bytes.NewReader(dec), binary.LittleEndian, &a.hashes); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	// Decrypt block table.
+	const blockEntrySize = 16
+	n = blockEntrySize * int64(a.BlockCount)
+	enc = make([]byte, n)
+	if _, err := a.r.ReadAt(enc, int64(a.BlockTableOffset)); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	dec = decrypt(enc, blockTableKey)
+
+	// Parse block table.
+	a.blocks = make([]blockEntry, a.BlockCount)
+	if err := binary.Read(bytes.NewReader(dec), binary.LittleEndian, &a.blocks); err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return a, nil
 }
 
 // ReadFile returns the contents of the given file in the archive.
@@ -18,4 +86,107 @@ func (a *Archive) ReadFile(relPath string) ([]byte, error) {
 // Files returns the list of file paths contained within the archive.
 func (a *Archive) Files() []string {
 	panic("not yet implemented")
+}
+
+// A header is an MPQ header.
+type header struct {
+	// Magic signature; "MPQ\x1A"
+	Magic [4]byte
+	// Size in bytes of the MPQ header.
+	HeaderSize uint32
+	// Size in bytes of the MPQ archive.
+	ArchiveSize uint32
+	// MPQ format version.
+	FormatVersion uint16
+	// Sector size exponent.
+	//
+	//    sectorSize = 512 * 2^SectorSizeExp.
+	SectorSizeExp uint16
+	// Offset to the start of the hash table.
+	HashTableOffset uint32
+	// Offset to the start of the block table.
+	BlockTableOffset uint32
+	// Number of hash table entires.
+	HashCount uint32
+	// Number of block table entires.
+	BlockCount uint32
+}
+
+// A hashEntry is a hash table entry.
+type hashEntry struct {
+	// Hash of relative file path, using method A.
+	HashA uint32
+	// Hash of relative file path, using method B.
+	HashB uint32
+	// Language locale of the file.
+	Locale uint16
+	// Platform of the file.
+	Platform uint16
+	// Index into the block table for the file; or one of the following two
+	// values.
+	//
+	//    0xFFFFFFFF   empty hash table entry; terminate search for file.
+	//    0xFFFFFFFE   empty hash table entry; continue search for file.
+	BlockIndex uint32
+}
+
+// A blockEntry is a block table entry.
+type blockEntry struct {
+	// Offset to the start of the block.
+	BlockOffset uint32
+	// Size in bytes of the block.
+	BlockSize uint32
+	// Size in bytes of the uncompressed file.
+	FileSize uint32
+	// Flags associated with the block.
+	Flags blockFlag
+}
+
+// blockFlag represents the set of block flags.
+type blockFlag uint32
+
+// Block flags.
+const (
+	fileImplode    blockFlag = 0x00000100 // compressed file (PKWARE)
+	fileCompress   blockFlag = 0x00000200 // compressed file
+	fileEncrypted  blockFlag = 0x00010000 // encrypted file
+	filePosDepKey  blockFlag = 0x00020000 // position dependent decryption key
+	filePatchFile  blockFlag = 0x00100000 // incremental patch file for an existing file in the base MPQ
+	fileSingleUnit blockFlag = 0x01000000 // file contained in a single unit, not split in sections
+	fileDeleted    blockFlag = 0x02000000 // deleted file; used by patch archives to delete files in lower-priority archives
+	fileSectorCRC  blockFlag = 0x04000000 // sector CRC checksums
+	fileExists     blockFlag = 0x80000000 // file present
+)
+
+// String returns the string representation of the block flags.
+func (flags blockFlag) String() string {
+	var ss []string
+	masks := map[blockFlag]string{
+		fileImplode:    "IMPLODE",
+		fileCompress:   "COMPRESS",
+		fileEncrypted:  "ENCRYPTED",
+		filePosDepKey:  "POSITION_DEPENDENT_KEY",
+		filePatchFile:  "PATCH_FILE",
+		fileSingleUnit: "SINGLE_UNIT",
+		fileDeleted:    "DELETED",
+		fileSectorCRC:  "SECTOR_CRC",
+		fileExists:     "EXISTS",
+	}
+	var keys []blockFlag
+	for key := range masks {
+		keys = append(keys, key)
+	}
+	less := func(i, j int) bool {
+		return keys[i] < keys[j]
+	}
+	sort.Slice(keys, less)
+	for _, mask := range keys {
+		if flags&mask != 0 {
+			ss = append(ss, masks[mask])
+		}
+	}
+	if len(ss) == 0 {
+		return "<no block flags>"
+	}
+	return strings.Join(ss, " | ")
 }
