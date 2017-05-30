01572115ea0e827dea5162235f2091cafefef21e
mewmew <rnd0x00@gmail.com>
2017-05-30 21:12:48 +0200

    d1: simplify parseString

--- d1/parse.go
+++ d1/parse.go
@@ -1,6 +1,7 @@
 package d1
 
 import (
+	"bytes"
 	"fmt"
 
 	"github.com/decomp/exp/bin"
@@ -13,12 +14,11 @@ func (exe *Executable) parseString(addr uint32) string {
 		return ""
 	}
 	data := exe.file.Data(bin.Address(addr))
-	for i, b := range data {
-		if b == 0 {
-			return string(data[:i])
-		}
+	pos := bytes.IndexByte(data, '\x00')
+	if pos == -1 {
+		panic(fmt.Errorf("unable to locate NULL-terminated string at address %q", addr))
 	}
-	panic(fmt.Errorf("unable to locate NULL-terminated string at address %q", addr))
+	return string(data[:pos])
 }
 
 // translateBool32 returns a Go version of the given 32-bit boolean value.
